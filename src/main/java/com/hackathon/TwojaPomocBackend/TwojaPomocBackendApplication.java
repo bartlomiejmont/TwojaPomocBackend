9f63994bc86abe81d35c76c100d51940e59783c5
Bartek <bartlomiejmont@gmail.com>
2020-04-04 17:57:19 +0200

    initial commit

---
+++ src/main/java/com/hackathon/TwojaPomocBackend/TwojaPomocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.TwojaPomocBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojaPomocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwojaPomocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/TwojaPomocBackend/TwojaPomocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.TwojaPomocBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojaPomocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
