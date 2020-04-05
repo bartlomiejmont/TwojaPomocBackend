7a8ae99bae9ed47ec57fb53e9f8d990e54636d43
Bartek <bartlomiejmont@gmail.com>
2020-04-05 04:16:25 +0200

    cors

--- src/main/java/com/hackathon/TwojaPomocBackend/TwojaPomocBackendApplication.java
+++ src/main/java/com/hackathon/TwojaPomocBackend/TwojaPomocBackendApplication.java
@@ -1,16 +1,27 @@
 package com.hackathon.TwojaPomocBackend;
 
-import com.hackathon.TwojaPomocBackend.model.TestRequest;
-import com.hackathon.TwojaPomocBackend.repository.TestRequestRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TwojaPomocBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TwojaPomocBackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/requests").allowedOrigins("*");
+			}
+		};
 	}
 
 }
